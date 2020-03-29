e1e5e99d2cf0c776f83a887ed59e61da793e9655
gaomin-github <374204501@qq.com>
2020-03-29 14:40:28 +0800

    2020-03-29 1440 up and down

--- src/granules/index.vue
+++ src/granules/index.vue
@@ -1,197 +1,225 @@
 <template>
-    <section ref="renderDom"></section>
+  <section ref="renderDom"></section>
 </template>
 <script>
 import * as Three from "three";
 import OBJLoader from "three-obj-loader";
 export default {
-    data() {
-        return {
-            scene: null,
-            camera: null,
-            renderer: null,
-            fogNum: 0.01,
-            geometry: null,
-            particle: null,
-            particles: [],
-            group: null,
-            clock: null,
-            newMesh: null,
-            meshes: []
-        };
+  data() {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      fogNum: 0.01,
+      geometry: null,
+      particle: null,
+      particles: [],
+      group: null,
+      clock: null,
+      newMesh: null,
+      meshes: [],
+      dir: 1 //向上
+    };
+  },
+  mounted() {
+    document.title = "obj loader";
+    this._initScene();
+    // this._initCube();
+    this._getObjData();
+    // this._renderAnimate();
+  },
+  methods: {
+    _initScene() {
+      //   相机
+      //   this.camera = new Three.OrthographicCamera(
+      //     window.innerWidth / -20,
+      //     window.innerWidth / 20,
+      //     window.innerHeight / 20,
+      //     window.innerHeight / -20,
+      //     1,
+      //     1000
+      //   );
+      //   this.camera.position.set(300, -300, 300);
+
+      //   this.camera.lookAt(new Three.Vector3(0, 0, 0));
+      this.camera = new Three.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1,
+        50000
+      );
+      this.camera.position.set(0, 70, 700);
+      //   场景
+      this.scene = new Three.Scene();
+      this.scene.add(new Three.AxisHelper(55));
+
+      let dirLight = new Three.DirectionalLight(0xffffff, 1);
+      dirLight.position.set(8, 10, 6);
+      this.scene.add(dirLight);
+      // 计时器
+      this.clock = new Three.Clock();
+
+      //   渲染器
+      this.renderer = new Three.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      let renDom = this.$refs.renderDom;
+      renDom.appendChild(this.renderer.domElement);
     },
-    mounted() {
-        document.title = "obj loader";
-        this._initScene();
-        // this._initCube();
-        this._getObjData();
-        // this._renderAnimate();
+    _initCube() {
+      let cube = new Three.BoxBufferGeometry(1, 1, 1);
+      let materialCube = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+      let cubeMesh = new Three.Mesh(cube, materialCube);
+      this.scene.add(cubeMesh);
     },
-    methods: {
-        _initScene() {
-            //   相机
-            this.camera = new Three.OrthographicCamera(
-                window.innerWidth / -20,
-                window.innerWidth / 20,
-                window.innerHeight / 20,
-                window.innerHeight / -20,
-                1,
-                1000
-            );
-            this.camera.position.set(300, 300, 300);
-            // this.camera.position.set(0, 0, 0);
+    _getObjData() {
+      OBJLoader(Three);
+      let loader = new Three.OBJLoader();
+      //   import("./jkm_female1.obj").then(module => {
+      //     console.log(`module`);
+      //     console.log(module);
+      //   });
+      //   loader.load("jkm_female1.obj", module => {
+      loader.load("male02.obj", module => {
+        this.group = new Three.Group();
+        this.scene.add(this.group);
+        // this.geometry = module.children[1].geometry;
 
-            this.camera.lookAt(new Three.Vector3(0, 0, 0));
+        // let positions = this.geometry.attributes.position;
+        // for (let i = 0; i < positions.length; i++) {
+        // sprite实现
+        // let material = new Three.SpriteMaterial({
+        //     color: 0x080808 * Math.random() + 0x080808,
+        //     size: 2
+        // });
+        // let particle = new Three.Sprite(material);
+        // particle.position.x = positions.getX(i);
+        // particle.position.y = -10;
+        // particle.position.z = positions.getZ(i);
+        // this.group.add(particle);
+        // this.particles.push(particle);
+        // }
+        // point实现
+        let clones = [
+          [60, 0, -40],
+          [50, 0, 0],
+          [10, 0, 50],
+          //   [10, 0, -50],
+          //   [40, 0, 20],
+          //   [-40, 0, 10],
+          //   [-50, 0, -50],
 
-            //   场景
-            this.scene = new Three.Scene();
-            this.scene.add(new Three.AxisHelper(15));
+          [0, 0, 0]
+        ];
+        let mesh = null;
+        // console.log(`module.length:${module.children.length}`);
+        for (let i = 0; i < module.children.length; i++) {
+          //   for (let m = 0; m < clones.length; m++) {
+          let geometry = module.children[i].geometry;
+          geometry.attributes.position.setUsage(Three.DynamicDrawUsage);
+          let positions = geometry.attributes.position;
+          geometry.setAttribute("initPosition", positions.clone());
+          for (let j = 0; j < positions.count; j++) {
+            positions.setY(j, 0);
+          }
+          mesh = new Three.Points(
+            geometry,
+            new Three.PointsMaterial({
+              size: 3,
+              color: 0xffffff
+            })
+          );
+          //   mesh.position.x = clones[m][0];
+          //   mesh.position.y = 0;
+          //   mesh.position.z = clones[m][2];
+          //   mesh.rotation.y = 10;
+          mesh.position.x = 30;
+          mesh.position.y = 0;
+          mesh.position.z = 30;
 
-            let dirLight = new Three.DirectionalLight(0xffffff, 1);
-            dirLight.position.set(8, 10, 6);
-            this.scene.add(dirLight);
-            // 计时器
-            this.clock = new Three.Clock();
-
-            //   渲染器
-            this.renderer = new Three.WebGLRenderer({ antialias: true });
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-            let renDom = this.$refs.renderDom;
-            renDom.appendChild(this.renderer.domElement);
-        },
-        _initCube() {
-            let cube = new Three.BoxBufferGeometry(1, 1, 1);
-            let materialCube = new Three.MeshBasicMaterial({ color: 0x00ff00 });
-            let cubeMesh = new Three.Mesh(cube, materialCube);
-            this.scene.add(cubeMesh);
-        },
-        _getObjData() {
-            OBJLoader(Three);
-            let loader = new Three.OBJLoader();
-            //   import("./jkm_female1.obj").then(module => {
-            //     console.log(`module`);
-            //     console.log(module);
-            //   });
-            loader.load("jkm_female1.obj", module => {
-                console.log(`obj`, 49);
-                console.log(module);
-                this.scene.add(module);
-                this.geometry = module.children[1].geometry;
-                // console.log(, 81);
-                this.group = new Three.Group();
-                this.scene.add(this.group);
-
-                let positions = this.geometry.attributes.position;
-                // for (let i = 0; i < positions.length; i++) {
-                // sprite实现
-                // let material = new Three.SpriteMaterial({
-                //     color: 0x080808 * Math.random() + 0x080808,
-                //     size: 2
-                // });
-                // let particle = new Three.Sprite(material);
-                // particle.position.x = positions.getX(i);
-                // particle.position.y = -10;
-                // particle.position.z = positions.getZ(i);
-                // this.group.add(particle);
-                // this.particles.push(particle);
-                // }
-                // point实现
-                for (let i = 0; i < module.children.length; i++) {
-                    let geometry = module.children[i].geometry;
-                    let positions = geometry.attributes.position;
-                    geometry.setAttribute("initPosition", positions.clone());
-                    for (let j = 0; j < positions.count; j++) {
-                        positions.setY(j, 0);
-                    }
-                    let mesh = new Three.Points(
-                        geometry,
-                        new Three.PointsMaterial({
-                            size: 3,
-                            color: 0xffffff
-                        })
-                    );
-                    mesh.position.x = 0;
-                    mesh.position.y = 10;
-                    mesh.position.z = 0;
-                    // this.group.add(mesh);
-                    this.meshes.push(mesh);
-                }
-                // this.geometry.setAttribute(
-                //     "initialPosition",
-                //     positions.clone()
-                // );
-                // this.newMesh = new Three.Points(
-                //     this.geometry,
-                //     new Three.PointsMaterial({
-                //         size: 5,
-                //         color: 0x808008 * Math.random() + 0x808008
-                //     })
-                // );
-                // let newMeshPositions = this.newMesh.geometry.attributes
-                //     .position;
-                // for (let i = 0; i < newMeshPositions.count; i++) {
-                //     newMeshPositions.setY(i, -5);
-                // }
-                // this.newMesh.position.x = 0;
-                // this.newMesh.position.y = -10;
-                // this.newMesh.position.z = 0;
-                // console.log(`this.new Mesh init`, 114);
-                // this.group.add(this.newMesh);
-                this._renderAnimate();
-            });
-        },
-        _renderAnimate() {
-            console.log(`renderAnimation`, 119);
-            if (this.fogNum > 0) {
-                this.fogNum -= 0.0001;
-
-                // this.scene.fog = new Three.FogExp2("#fff", this.fogNum);
-            }
-            // let positions = this.geometry.attributes.position;
-            // for (let i = 0; i < this.particles.length; i++) {
-            //     let particle = this.particles[i];
-
-            //     if (particle.position.y < positions.getY(i)) {
-            //         this.particles[i].position.y += 0.2;
-            //     }
-            // }
-            // this.newMesh.rotation.y += 0.01;
-            for (let i = 0; i < this.meshes.length; i++) {
-                let mesh = this.meshes[i];
-
-                let meshPositions = mesh.geometry.attributes.position;
-                let initMeshPositions = mesh.geometry.attributes.initPosition;
-                for (let j = 0; j < meshPositions.count; j++) {
-                    let px = meshPositions.getX(j);
-                    let py = meshPositions.getY(j);
-                    let pz = meshPositions.getZ(j);
-
-                    // console.log(
-                    //     `py:${py}:initial:${initMeshPositions.getY(i)}`
-                    // );
-                    if (py < initMeshPositions.getY(j)) {
-                        meshPositions.setXYZ(j, px, py + 0.1, pz);
-                    }
-                    // let pz = meshPositions.getZ(i);
-                    // meshPositions.setXYZ(
-                    //     i,
-                    //     px + 10 * Math.random(),
-                    //     px + 0.5 * Math.random(),
-                    //     px + 0.5 * Math.random()
-                    // );
-                }
-                meshPositions.needsUpdate = true;
-            }
-            // let count = meshPositions.count;
-
-            // this.newMesh.updateMorphTargets();
-            console.log(`renderAnimation`, 155);
-
-            this.renderer.render(this.scene, this.camera);
-            // setTimeout(this._renderAnimate, 1000);
-            requestAnimationFrame(this._renderAnimate);
+          this.group.add(mesh);
+          //   }
+          this.meshes.push(mesh);
         }
+        this._renderAnimate();
+      });
+    },
+    _renderAnimate() {
+      //   console.log(`renderAnimation`, 119);
+      if (this.fogNum > 0) {
+        this.fogNum -= 0.0001;
+
+        // this.scene.fog = new Three.FogExp2("#fff", this.fogNum);
+      }
+      //   let positions = this.geometry.attributes.position;
+      //   for (let i = 0; i < this.particles.length; i++) {
+      //     let particle = this.particles[i];
+
+      //     if (particle.position.y < positions.getY(i)) {
+      //       this.particles[i].position.y += 0.2;
+      //     }
+      //   }
+      //   this.group.rotation.y -= 0.002;
+      let verticalCount = 0;
+      let allPointCount = 0;
+      for (let i = 0; i < this.meshes.length; i++) {
+        let mesh = this.meshes[i];
+        let meshPositions = mesh.geometry.attributes.position;
+        allPointCount += meshPositions.count;
+      }
+      //   console.log(this.meshes);
+      let delta = this.clock.getDelta() * 100;
+      for (let i = 0; i < this.meshes.length; i++) {
+        let mesh = this.meshes[i];
+
+        let meshPositions = mesh.geometry.attributes.position;
+        let initMeshPositions = mesh.geometry.attributes.initPosition;
+        for (let j = 0; j < meshPositions.count; j++) {
+          let px = meshPositions.getX(j);
+          let py = meshPositions.getY(j);
+          let pz = meshPositions.getZ(j);
+          //   console.log(`py:${py},initPy:${initMeshPositions.getY(j)}`);
+          if (this.dir > 0) {
+            //   向上
+            let ix = initMeshPositions.getX(i);
+            let iy = initMeshPositions.getY(i);
+            let iz = initMeshPositions.getZ(i);
+
+            let dx = Math.abs(px - ix);
+            let dy = Math.abs(py - iy);
+            let dz = Math.abs(pz - iz);
+
+            if (py < initMeshPositions.getY(j)) {
+              meshPositions.setXYZ(j, px, py + delta * Math.random(), pz);
+            } else {
+              verticalCount++;
+            }
+            if (verticalCount === allPointCount) this.dir = -1;
+          } else {
+            //   向下
+            if (py > 0) {
+              let iy = initMeshPositions.getY(i);
+
+              meshPositions.setXYZ(
+                j,
+                px + (0.5 - Math.random()) * delta,
+                // px,
+                py + (0.25 - Math.random()) * delta,
+                // pz
+                pz + (0.5 - Math.random()) * delta
+              );
+            } else {
+              verticalCount++;
+            }
+            if (verticalCount === allPointCount) this.dir = 1;
+          }
+        }
+        meshPositions.needsUpdate = true;
+      }
+
+      this.renderer.render(this.scene, this.camera);
+      //   requestAnimationFrame(this._renderAnimate);
     }
+  }
 };
 </script>
 <style lang="scss" scoped></style>
