2ece90d574d2582fd58eebc1cf51d2399465e1f0
gaomin-github <374204501@qq.com>
2020-04-24 20:09:03 +0800

    2020-04-24 2009 update

--- src/lottieDemo/index.vue
+++ src/lottieDemo/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="animate-wrapper">
-        this is lottie img test
+        this is lottie img test 123
         <section v-for="(item, key) in jsUrlList" :key="key">
             <lottie-component :jsUrl="item" class="img-item"></lottie-component>
         </section>
