f67ec82dbe1ec2d9876fad070a3e7a3d378456a3
gaomin-github <374204501@qq.com>
2020-05-12 01:01:40 +0800

    2020-05-11 0101 增加图片上传

--- src/xian-estate/assess-img.vue
+++ src/xian-estate/assess-img.vue
@@ -1,49 +1,56 @@
 <template>
-    <div class="assess-img-wrapper">
-        <div v-if="imgUrl"></div>
-        <div v-else class="assess-img" @click="chooseImg"></div>
-    </div>
+  <div class="assess-img-wrapper">
+    <img v-if="data" :src="data" class="assess-img" />
+    <div v-else class="assess-bg" @click="chooseImg">+</div>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        imgUrl: ""
+  props: {
+    data: "",
+  },
+  methods: {
+    chooseImg() {
+      //   console.log("chooseImg");
+      this.$emit("chooseImg");
     },
-    methods: {
-        chooseImg() {
-            this.$emit("chooseImg");
-        }
-    }
+  },
 };
 </script>
 <style lang="scss" scoped>
 @import url("https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css");
 section,
 div {
-    display: block;
-    overflow: hidden;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  display: block;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 .assess-img-wrapper {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 .assess-img {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    line-height: 100%;
-    font-size: 70px;
-    font-weight: 200;
-    color: #999;
-    border: 1px black solid;
-    padding-bottom: 10px;
-    &:before {
-        content: "+";
-    }
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.assess-bg {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  line-height: 100%;
+  font-size: 50px;
+  font-weight: 200;
+  color: #999;
+  border: 1px #999 solid;
+  padding-bottom: 10px;
+  &:before {
+    /* content: "+"; */
+    display: inline;
+  }
 }
 </style>
