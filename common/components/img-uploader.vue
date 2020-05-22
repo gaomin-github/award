663c307148ed2bf3b2bc335c2003d5c37ad62e18
gaomin-github <374204501@qq.com>
2020-05-23 00:29:19 +0800

    2020-05-22 0029 修改图片上传组件

--- common/components/img-uploader.vue
+++ common/components/img-uploader.vue
@@ -1,193 +1,206 @@
 <template>
-    <div class="img-uploader-wrapper">
-        <img
-            v-if="imgUrl"
-            :src="imgUrl"
-            :class="[`assess-img`,showDel?'assess-img-del':``]"
-            @touchstart="handleTouchStart"
-            @touchend="handleTouchEnd"
-        />
-        <div v-if="!imgUrl" class="assess-bg" @click="chooseImg">+</div>
-        <input
-            v-if="!imgUrl"
-            type="file"
-            accept="image/*"
-            style="opacity:0"
-            ref="imgPicker"
-            multiple="multiple"
-            @change="changeImg"
-        />
-        <div class="loading" v-if="showLoading">
-            <ui-progress :value="loadingValue"></ui-progress>
-        </div>
-        <div class="del" v-if="showDel">
-            <i class="icon fa fa-times" @click="handleDropFile"></i>
-        </div>
+  <div class="img-uploader-wrapper">
+    <img
+      v-if="imgUrl"
+      :src="imgUrl"
+      :class="[`assess-img`, showDel ? 'assess-img-del' : ``]"
+      @touchstart="handleTouchStart"
+      @touchend="handleTouchEnd"
+    />
+    <div v-if="!imgUrl" class="assess-bg" @click="chooseImg">+</div>
+    <input
+      v-if="!imgUrl"
+      type="file"
+      accept="image/*"
+      style="opacity:0"
+      ref="imgPicker"
+      multiple="multiple"
+      @change="changeImg"
+    />
+    <div class="loading" v-if="showLoading">
+      <ui-progress :value="loadingValue"></ui-progress>
     </div>
+    <div class="del" v-if="showDel">
+      <i class="icon fa fa-times" @click="handleDropFile"></i>
+    </div>
+  </div>
 </template>
 <script>
 import uiProgress from "./ui-progress.vue";
 const DeleteDelay = 1000;
 export default {
-    components: {
-        uiProgress
+  components: {
+    uiProgress,
+  },
+  props: {
+    imgUrl: String,
+  },
+  data() {
+    return {
+      showLoading: false,
+      loadingValue: 0,
+      timerTask: null,
+      touchStartTime: 0,
+      showDel: false,
+    };
+  },
+  watch: {
+    imgUrl(newValue, oldValue) {
+      console.log("49");
+      console.log(newValue, 49);
     },
-    props: {
-        imgUrl: ""
-    },
-    data() {
-        return {
-            showLoading: false,
-            loadingValue: 0,
-            timerTask: null,
-            touchStartTime: 0,
-            showDel: false
-        };
-    },
-    mounted() {},
-    methods: {
-        chooseImg() {
-            this.$refs.imgPicker.click();
-        },
-        changeImg(e) {
-            this.showLoading = true;
-            let imgs = [];
-            let imgObjs = this.$refs.imgPicker.files;
-            if (!imgObjs || imgObjs.length === 0) return;
-            for (let index in imgObjs) {
-                // if (index >= imgObjs.length) return;
-                if (index < imgObjs.length) {
-                    console.log(imgObjs, 63);
-                    console.log(index, 60);
-                    console.log(imgObjs[index], 61);
-                    let reader = new FileReader();
-                    let imgObj = imgObjs[index];
-                    reader.readAsDataURL(imgObj);
-                    reader.onload = () => {
-                        let imgObject = {
-                            id: Math.random(),
-                            data: reader.result,
-                            fileObj: imgObj
-                        };
-                        imgs.push(imgObject);
-                        // console.log(imgs, 60);
-                        this.loadingValue = imgs.length / imgObjs.length;
-                        if (imgs.length === imgObjs.length) {
-                            this.$emit("chooseImg", imgs);
-                            requestAnimationFrame(() => {
-                                this.showLoading = false;
-                                this.loadingValue = 0;
-                            });
-                        }
-                    };
-                }
-            }
-        },
-        handleTouchStart() {
-            if (!this.imgUrl) return;
-            this.touchStartTime = new Date().getTime();
-            // console.log("touchStart", this.touchStartTime);
-            this.timerTask = setTimeout(() => {
-                this.showDel = true;
-                // console.log(this.showDel, 88);
-            }, DeleteDelay);
-        },
-        handleTouchEnd() {
-            // console.log("touchend", new Date().getTime());
+  },
+  mounted() {
+    console.log(this.imgUrl, 54);
+    console.log(this.showLoading, 55);
 
-            if (new Date().getTime() - this.touchStartTime < DeleteDelay) {
-                this.showDel = false;
-                clearTimeout(this.timerTask);
-                this.timerTask = null;
-            }
-            // console.log(this.showDel, 98);
-        },
-        handleDropFile() {
-            this.$emit("dropImg", this.imgUrl);
+    // if (this.imgUrl) {
+    this.showLoading = false;
+    // }
+  },
+  methods: {
+    chooseImg() {
+      this.$refs.imgPicker.click();
+    },
+    changeImg(e) {
+      this.showLoading = true;
+      let imgs = [];
+      let imgObjs = this.$refs.imgPicker.files;
+      if (!imgObjs || imgObjs.length === 0) return;
+      for (let index in imgObjs) {
+        // if (index >= imgObjs.length) return;
+        if (index < imgObjs.length) {
+          //   console.log(imgObjs, 63);
+          //   console.log(index, 60);
+          //   console.log(imgObjs[index], 61);
+          //   let reader = new FileReader();
+          let imgObj = imgObjs[index];
+          //   reader.readAsDataURL(imgObj);
+          //   reader.onload = () => {
+          let imgObject = {
+            id: Math.random(),
+            //   data: reader.result,
+            fileObj: imgObj,
+          };
+          imgs.push(imgObject);
+          // console.log(imgs, 60);
+          this.loadingValue = imgs.length / imgObjs.length;
+          if (imgs.length === imgObjs.length) {
+            this.$emit("chooseImg", imgs);
+            requestAnimationFrame(() => {
+              // this.showLoading = false;
+              this.loadingValue = 0;
+            });
+          }
+          //   };
         }
-    }
+      }
+    },
+    handleTouchStart() {
+      if (!this.imgUrl) return;
+      this.touchStartTime = new Date().getTime();
+      // console.log("touchStart", this.touchStartTime);
+      this.timerTask = setTimeout(() => {
+        this.showDel = true;
+        // console.log(this.showDel, 88);
+      }, DeleteDelay);
+    },
+    handleTouchEnd() {
+      // console.log("touchend", new Date().getTime());
+
+      if (new Date().getTime() - this.touchStartTime < DeleteDelay) {
+        this.showDel = false;
+        clearTimeout(this.timerTask);
+        this.timerTask = null;
+      }
+      // console.log(this.showDel, 98);
+    },
+    handleDropFile() {
+      this.$emit("dropImg", this.imgUrl);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 @import url("https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css");
 section,
 div {
-    display: block;
-    overflow: hidden;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  display: block;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 .img-uploader-wrapper {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    border-radius: 10px;
-    overflow: visible;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  border-radius: 10px;
+  overflow: visible;
 }
 .loading {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    bottom: 0px;
-    padding: 0px 20px;
-    background: #eee;
-    border-radius: 10px;
-    border: 1px #ccc solid;
-    display: flex;
-    align-items: center;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  padding: 0px 20px;
+  background: #eee;
+  border-radius: 10px;
+  border: 1px #ccc solid;
+  display: flex;
+  align-items: center;
 }
 .del {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  overflow: visible !important;
+  .icon {
+    display: block;
     position: absolute;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    bottom: 0px;
-    overflow: visible !important;
-    .icon {
-        display: block;
-        position: absolute;
-        box-sizing: border-box;
-        top: -30px;
-        right: -30px;
-        width: 65px;
-        height: 55px;
-        padding: 20px 20px 0px 20px;
-        color: #999;
-        font-size: 16px;
-        font-weight: 300;
-    }
+    box-sizing: border-box;
+    top: -30px;
+    right: -30px;
+    width: 65px;
+    height: 55px;
+    padding: 20px 20px 0px 20px;
+    color: #999;
+    font-size: 16px;
+    font-weight: 300;
+  }
 }
 
 .assess-img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    transition: all 0.3s ease;
-    // width: 70%;
-    // height: 70%;
-    // transform: translate(15%, 15%);
+  display: block;
+  width: 100%;
+  height: 100%;
+  transition: all 0.3s ease;
+  // width: 70%;
+  // height: 70%;
+  // transform: translate(15%, 15%);
 }
 .assess-img-del {
-    width: 80%;
-    height: 80%;
-    transform: translate(12%, 12%);
+  width: 80%;
+  height: 80%;
+  transform: translate(12%, 12%);
 }
 .assess-bg {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    line-height: 100%;
-    font-size: 50px;
-    font-weight: 200;
-    background: #eee;
-    color: #999;
-    &:before {
-        /* content: "+"; */
-        display: inline;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  line-height: 100%;
+  font-size: 50px;
+  font-weight: 200;
+  background: #eee;
+  color: #999;
+  &:before {
+    /* content: "+"; */
+    display: inline;
+  }
 }
 </style>
--- src/weekly/com/editor.vue
+++ src/weekly/com/editor.vue
@@ -1,71 +1,78 @@
 <template>
-    <div class="editor-wrapper">
-        <header-tool @back="back" />
-        <div class="container">
-            <div class="grade">
-                <div class="grade-progress">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105 10">
-                        <line class="grade-progress-path" x1="5" y1="5" x2="100" y2="5" />
-                        <line
-                            class="grade-progress-path valid"
-                            v-if="process && worth"
-                            x1="5"
-                            y1="5"
-                            x2="100"
-                            :stroke-dasharray="
+  <div class="editor-wrapper">
+    <header-tool @back="back" />
+    <div class="container">
+      <div class="grade">
+        <div class="grade-progress">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105 10">
+            <line class="grade-progress-path" x1="5" y1="5" x2="100" y2="5" />
+            <line
+              class="grade-progress-path valid"
+              v-if="process && worth"
+              x1="5"
+              y1="5"
+              x2="100"
+              :stroke-dasharray="
                 `${process < worth ? (100 * process) / worth : 100} 500`
               "
-                            y2="5"
-                        />
-                    </svg>
-                </div>
-                <div class="grade-worth">
-                    总分
-                    <input type="number" v-model="worth" />
-                </div>
-                <div class="grade-process">
-                    ,现得分
-                    <span>
-                        <input type="number" v-model="process" />
-                    </span>
-                </div>
-            </div>
-            <textarea
-                ref="content"
-                class="content"
-                v-model="content"
-                @input="inputHandler"
-                placeholder="add some content"
-            ></textarea>
-            <div class="schedule">
-                <div v-if="curPicPath && curPicPath.length > 0" class="schedule-cur">
-                    <img-uploader
-                        v-for="(pic) in curPicPath"
-                        :key="pic"
-                        class="schedule-cur-item"
-                        @chooseImg="chooseImg"
-                        @dropImg="deleteImg"
-                        :imgUrl="pic"
-                    ></img-uploader>
-                    <img-uploader class="schedule-cur-item" @chooseImg="chooseImg"></img-uploader>
-                </div>
-                <img-uploader class="schedule-cur-item" @chooseImg="chooseImg" v-else></img-uploader>
-            </div>
-            <div class="schedule history" v-if="pics&&pics.length>0">
-                <div class="history-item" v-for="(pic,picKey) in pics" :key="picKey">
-                    <div class="history-item-time">{{pic.createTime}}</div>
-                    <div class="history-item-pics" v-if="pic.urls&&pic.urls.length>0">
-                        <img-uploader
-                            v-for="url in pic.urls"
-                            :key="url"
-                            class="history-item-pics-item"
-                            :imgUrl="url"
-                        ></img-uploader>
-                    </div>
-                </div>
-            </div>
+              y2="5"
+            />
+          </svg>
         </div>
+        <div class="grade-worth">
+          总分
+          <input type="number" v-model="worth" />
+        </div>
+        <div class="grade-process">
+          ,现得分
+          <span>
+            <input type="number" v-model="process" />
+          </span>
+        </div>
+      </div>
+      <textarea
+        ref="content"
+        class="content"
+        v-model="content"
+        @input="inputHandler"
+        placeholder="add some content"
+      ></textarea>
+      <div class="schedule">
+        <div v-if="curPicPath && curPicPath.length > 0" class="schedule-cur">
+          <img-uploader
+            v-for="pic in curPicPath"
+            :key="pic"
+            class="schedule-cur-item"
+            @chooseImg="chooseImg"
+            @dropImg="deleteImg"
+            :imgUrl="pic"
+          ></img-uploader>
+          <img-uploader
+            class="schedule-cur-item"
+            @chooseImg="chooseImg"
+          ></img-uploader>
+        </div>
+        <img-uploader
+          class="schedule-cur-item"
+          @chooseImg="chooseImg"
+          v-else
+        ></img-uploader>
+      </div>
+      <div class="schedule history" v-if="pics && pics.length > 0">
+        <div class="history-item" v-for="(pic, picKey) in pics" :key="picKey">
+          <div class="history-item-time">{{ pic.createTime }}</div>
+          <div class="history-item-pics" v-if="pic.urls && pic.urls.length > 0">
+            <img-uploader
+              v-for="url in pic.urls"
+              :key="url"
+              class="history-item-pics-item"
+              :imgUrl="url"
+            ></img-uploader>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import request from "request";
@@ -75,247 +82,244 @@ import { ossBatchUploader } from "lib/throttle.js";
 
 import imgUploader from "components/img-uploader.vue";
 export default {
-    components: {
-        headerTool: () => import("./headerTool.vue"),
-        imgUploader: () => import("components/img-uploader")
-    },
-    data() {
-        return {
-            content: "",
-            worth: 0,
-            process: 0,
-            pics: [],
-            curPics: [],
-            curPicPath: []
-        };
-    },
-    computed: {
-        ...mapState("weekly", [
-            "scheduleList",
-            "taskId",
-            "curSchedule",
-            "subId"
-        ])
-    },
-    mounted() {
-        this._prevData();
-        this.$refs.content.focus();
-    },
-    beforeDestroy() {
-        this.back();
-    },
-    methods: {
-        ...mapMutations("weekly", ["updateSchedule", "updateEditing"]),
-        back() {
-            // 保存新上传的图片
-            if (this.curPicPath && this.curPicPath.length > 0) {
-                let nowDate = new Date();
-                let createTime = `${nowDate.getFullYear()}-${nowDate.getMonth() +
-                    1}-${nowDate.getDay()}`;
-                createTime = createTime.replace(/\b(?=\d(\s|\:|\-|\b))/g, 0);
-                this.pics.push({
-                    createTime,
-                    urls: this.curPicPath
-                });
-            }
-            this._saveSchedule();
-            this.updateEditing(false);
-        },
-        _prevData() {
-            this.content = this.curSchedule.content;
-            this.worth = this.curSchedule.worth;
-            this.process = this.curSchedule.process;
-            this.pics = this.curSchedule.pics;
-            console.log(this.pics, 128);
-        },
-        inputHandler() {
-            throttle(() => {
-                // 更新到vuex
-                this._saveSchedule();
-            }, 500)();
-        },
-        // 返回
-        handleBack() {
-            this._saveSchedule();
-            this.updateEditing(false);
-        },
-        _saveSchedule() {
-            // 更新store
-            this.updateSchedule(
-                Object.assign(this.curSchedule, {
-                    content: this.content,
-                    worth: this.worth,
-                    process: this.process,
-                    pics: this.pics
-                })
-            );
-            // 更新数据库
-            let scheduleStr = encodeURIComponent(
-                JSON.stringify(this.scheduleList)
-            );
-            request
-                .get(
-                    `/task/updateTask?taskId=${this.taskId}&scheduleStr=${scheduleStr}`
-                )
-                .then(res => {
-                    if (res.status === 200) {
-                        return res.data;
-                    }
-                });
-        },
-        async chooseImg(param) {
-            this.curPics.push(...param);
-            let formObj = new FormData();
-            param.map(o => {
-                formObj.append("file", o.fileObj);
-            });
+  components: {
+    headerTool: () => import("./headerTool.vue"),
+    imgUploader: () => import("components/img-uploader"),
+  },
+  data() {
+    return {
+      content: "",
+      worth: 0,
+      process: 0,
+      pics: [],
+      curPics: [],
+      curPicPath: [],
+    };
+  },
+  computed: {
+    ...mapState("weekly", ["scheduleList", "taskId", "curSchedule", "subId"]),
+  },
+  mounted() {
+    this._prevData();
+    this.$refs.content.focus();
+  },
+  beforeDestroy() {
+    this.back();
+  },
+  methods: {
+    ...mapMutations("weekly", ["updateSchedule", "updateEditing"]),
+    back() {
+      // 保存新上传的图片
+      console.log(this.curPicPath, 111);
+      if (this.curPicPath && this.curPicPath.length > 0) {
+        let nowDate = new Date();
+        let createTime = `${nowDate.getFullYear()}-${nowDate.getMonth() +
+          1}-${nowDate.getDay()}`;
+        createTime = createTime.replace(/\b(?=\d(\s|\:|\-|\b))/g, 0);
 
-            formObj.append("path", "pictures/weekly");
-            request({
-                url: "/common/file",
-                method: "post",
-                data: formObj,
-                headers: {
-                    "content-type": "multipart/form-data"
-                }
-            }).then(res => {
-                if (res.status === 200) {
-                    this.curPicPath.push(...res.data);
-                }
-            });
+        this.pics.push({
+          createTime,
+          urls: this.curPicPath,
+        });
+      }
+      this.curPicPath = [];
+
+      this._saveSchedule();
+      this.updateEditing(false);
+    },
+    _prevData() {
+      this.content = this.curSchedule.content;
+      this.worth = this.curSchedule.worth;
+      this.process = this.curSchedule.process;
+      this.pics = this.curSchedule.pics || [];
+      console.log(this.pics, 128);
+    },
+    inputHandler() {
+      throttle(() => {
+        // 更新到vuex
+        this._saveSchedule();
+      }, 500)();
+    },
+    // 返回
+    // handleBack() {
+    //     this._saveSchedule();
+    //     this.updateEditing(false);
+    // },
+    _saveSchedule() {
+      // 更新store
+      this.updateSchedule(
+        Object.assign(this.curSchedule, {
+          content: this.content,
+          worth: this.worth,
+          process: this.process,
+          pics: this.pics,
+        })
+      );
+      // 更新数据库
+      let scheduleStr = encodeURIComponent(JSON.stringify(this.scheduleList));
+      request
+        .get(
+          `/task/updateTask?taskId=${this.taskId}&scheduleStr=${scheduleStr}`
+        )
+        .then((res) => {
+          if (res.status === 200) {
+            return res.data;
+          }
+        });
+    },
+    async chooseImg(param) {
+      this.curPics.push(...param);
+      let formObj = new FormData();
+      param.map((o) => {
+        formObj.append("file", o.fileObj);
+      });
+
+      formObj.append("path", "pictures/weekly");
+      request({
+        url: "/common/file",
+        method: "post",
+        data: formObj,
+        headers: {
+          "content-type": "multipart/form-data",
         },
-        deleteImg(param) {
-            request({
-                url: "/common/dropfile",
-                method: "post",
-                data: {
-                    filePath: param
-                }
-            }).then(res => {
-                if (res.status === 200 && res.data) {
-                    let index = this.curPicPath.findIndex(o => {
-                        return o === param;
-                    });
-                    this.curPicPath.splice(index, 1);
-                }
-            });
+      }).then((res) => {
+        if (res.status === 200) {
+          this.curPicPath.push(...res.data);
         }
-    }
+      });
+    },
+    deleteImg(param) {
+      request({
+        url: "/common/dropfile",
+        method: "post",
+        data: {
+          filePath: param,
+        },
+      }).then((res) => {
+        if (res.status === 200 && res.data) {
+          let index = this.curPicPath.findIndex((o) => {
+            return o === param;
+          });
+          this.curPicPath.splice(index, 1);
+        }
+      });
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 @keyframes valid_path {
-    // from{
-    //     stroke-dasharray: 0 0;
-    // }
-    to {
-        stroke-dasharray: 0 500;
-    }
+  // from{
+  //     stroke-dasharray: 0 0;
+  // }
+  to {
+    stroke-dasharray: 0 500;
+  }
 }
 .editor-wrapper {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    padding: 10px;
-    background: rgba(242, 245, 249, 1);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  padding: 10px;
+  background: rgba(242, 245, 249, 1);
 }
 textarea {
-    appearance: none;
-    border: none;
-    outline: none;
-    width: 100%;
-    height: 100%;
-    box-sizing: border-box;
+  appearance: none;
+  border: none;
+  outline: none;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
 }
 .container {
-    flex: 1;
-    flex-shrink: 1;
-    overflow-y: scroll;
+  flex: 1;
+  flex-shrink: 1;
+  overflow-y: scroll;
 }
 .grade {
-    height: 50px;
-    display: flex;
-    align-items: center;
-    &-progress {
-        flex: 1;
-        flex-shrink: 1;
-        svg {
-            width: 100%;
-            height: 100%;
-        }
-        &-path {
-            stroke-width: 5;
-            stroke-linecap: round;
-            stroke: #999;
-        }
-        .valid {
-            stroke: red;
-            transition: all 3s ease;
-            animation: valid_path 1s ease reverse;
-        }
+  height: 50px;
+  display: flex;
+  align-items: center;
+  &-progress {
+    flex: 1;
+    flex-shrink: 1;
+    svg {
+      width: 100%;
+      height: 100%;
     }
-    input[type="number"] {
-        appearance: none;
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        line-height: 30px;
-        font-size: 16px;
-        width: 36px;
-        border: none;
-        border-bottom: 1px #999 solid;
-        text-align: center;
-        background: none;
+    &-path {
+      stroke-width: 5;
+      stroke-linecap: round;
+      stroke: #999;
     }
-    &-worth {
-        flex-shrink: 1;
+    .valid {
+      stroke: red;
+      transition: all 3s ease;
+      animation: valid_path 1s ease reverse;
     }
-    &-process {
-        flex-shrink: 1;
-    }
+  }
+  input[type="number"] {
+    appearance: none;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    line-height: 30px;
+    font-size: 16px;
+    width: 36px;
+    border: none;
+    border-bottom: 1px #999 solid;
+    text-align: center;
+    background: none;
+  }
+  &-worth {
+    flex-shrink: 1;
+  }
+  &-process {
+    flex-shrink: 1;
+  }
 }
 .content {
-    height: 180px;
-    overflow-y: scroll;
-    padding: 0px 5px;
-    margin-top: 10px;
-    border: 1px rgba(180, 175, 175, 1) solid;
-    line-height: 28px;
-    font-size: 14px;
-    border-radius: 5px;
+  height: 180px;
+  overflow-y: scroll;
+  padding: 0px 5px;
+  margin-top: 10px;
+  border: 1px rgba(180, 175, 175, 1) solid;
+  line-height: 28px;
+  font-size: 14px;
+  border-radius: 5px;
 }
 .schedule {
-    background: rgba(255, 255, 255, 1);
-    &-item {
-        width: 100px;
-        height: 100px;
-    }
+  background: rgba(255, 255, 255, 1);
+  &-item {
+    width: 100px;
+    height: 100px;
+  }
 }
 .schedule-cur {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 
-    &-item {
-        margin: 10px 5px;
-        width: 100px;
-        height: 100px;
-    }
+  &-item {
+    margin: 10px 5px;
+    width: 100px;
+    height: 100px;
+  }
 }
 .history {
-    &-item {
-        &-time {
-            line-height: 30px;
-            font-size: 24px;
-        }
-        &-pics {
-            display: flex;
-            &-item {
-                width: 100px;
-                height: 100px;
-                margin: 10px 8px;
-            }
-        }
+  &-item {
+    &-time {
+      line-height: 30px;
+      font-size: 24px;
     }
+    &-pics {
+      display: flex;
+      &-item {
+        width: 100px;
+        height: 100px;
+        margin: 10px 8px;
+      }
+    }
+  }
 }
 </style>
