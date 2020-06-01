fbc9f4c838fe2ce3471ecc72e6767bb45d0538a4
gaomin-github <374204501@qq.com>
2020-06-01 17:28:31 +0800

    2020-06-01 1728 增加px转vw配置

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -88,7 +88,7 @@ module.exports = {
             },
             {
                 test: /\.(css|scss)$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: ["style-loader", "css-loader", "postcss-loader","sass-loader"],
                 exclude: /node_modules/,
                 include: [
                     path.resolve(__dirname, "../src"),
