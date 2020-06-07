5f90e7ee70063ea14eefc6b967b4ea638e0b4315
gaomin-github <374204501@qq.com>
2020-06-08 01:26:34 +0800

    2020-06-08 0126 增加vr页面

---
+++ src/vr/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="vr-wrapper"></div>
+</template>
+<script>
+const fileType = ["hall-of-frame", "pantry", "reception"];
+const dirType = ["back", "bottom", "front", "left", "right", "top"];
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
