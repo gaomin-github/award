1443f6f3238955d53b6b8923c61d5d312f69418c
gaomin-github <374204501@qq.com>
2020-06-26 23:32:47 +0800

    2020-0626-1132 完成jwt验证

--- src/login/index.vue
+++ src/login/index.vue
@@ -1,50 +1,63 @@
 <template>
   <div class="login-wrapper">
     <div class="content">
-      <!-- 注册模块 -->
-      <div v-for="(item, regIndex) in reg" :key="regIndex" class="horz">
-        <label class="horz-label">{{ item.label }}</label>
-        <template v-if="item.type === 'input'">
-          <input
-            type="text"
-            class="horz-content"
-            v-model="item.value"
-            :placeholder="item.placeHolder"
-          />
+      <template v-if="userInfo">
+        <div class="userInfo">欢迎用户：{{ userInfo.userName }}</div>
+      </template>
+      <template v-else>
+        <!-- 注册模块 -->
+        <template v-if="curCom === 'reg'">
+          <div v-for="(item, regIndex) in reg" :key="regIndex" class="horz">
+            <label class="horz-label">{{ item.label }}</label>
+            <template v-if="item.type === 'input'">
+              <input
+                type="text"
+                class="horz-content"
+                v-model="item.value"
+                :placeholder="item.placeHolder"
+              />
+            </template>
+            <template v-if="item.type === 'pwd'">
+              <input
+                type="password"
+                class="horz-content"
+                v-model="item.value"
+                :placeholder="item.placeHolder"
+              />
+            </template>
+          </div>
         </template>
-        <template v-if="item.type === 'pwd'">
-          <input
-            type="password"
-            class="horz-content"
-            v-model="item.value"
-            :placeholder="item.placeHolder"
-          />
+        <!-- 登陆模块 -->
+        <template v-if="curCom === 'login'">
+          <div v-for="(item, regIndex) in login" :key="regIndex" class="horz">
+            <label class="horz-label">{{ item.label }}</label>
+            <template v-if="item.type === 'input'">
+              <input
+                type="text"
+                class="horz-content"
+                v-model="item.value"
+                :placeholder="item.placeHolder"
+              />
+            </template>
+            <template v-if="item.type === 'pwd'">
+              <input
+                type="password"
+                class="horz-content"
+                v-model="item.value"
+                :placeholder="item.placeHolder"
+              />
+            </template>
+          </div>
         </template>
-      </div>
-      <!-- 登陆模块 -->
-      <div v-for="(item, regIndex) in reg" :key="regIndex" class="horz">
-        <label class="horz-label">{{ item.label }}</label>
-        <template v-if="item.type === 'input'">
-          <input
-            type="text"
-            class="horz-content"
-            v-model="item.value"
-            :placeholder="item.placeHolder"
-          />
-        </template>
-        <template v-if="item.type === 'pwd'">
-          <input
-            type="password"
-            class="horz-content"
-            v-model="item.value"
-            :placeholder="item.placeHolder"
-          />
-        </template>
-      </div>
-
-      <div class="submit-btn" @click="submitInfo">
-        {{ curCom === "reg" ? "注册" : "登陆" }}
-      </div>
+        <div class="btns">
+          <div class="btns-label" @click="changeComs">
+            {{ curCom === "reg" ? "已有账号，去登陆" : "新用户，去注册" }}
+          </div>
+          <div class="btns-submit" @click="submitInfo">
+            {{ curCom === "reg" ? "注册" : "登陆" }}
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -94,12 +107,29 @@ export default {
           value: "",
         },
       },
-      curCom: "reg",
+      curCom: "login",
+      userInfo: null,
     };
   },
+  async mounted() {
+    this._initUserInfo();
+    let loginStatus = await this._getLoginInfo();
+    if (!loginStatus) {
+      console.log("未登陆");
+      localStorage.clear();
+    } else {
+      console.log("一登陆");
+    }
+  },
   methods: {
-    changeComs(type) {
-      this.curCom = type;
+    _initUserInfo() {
+      let userInfo_str = localStorage.getItem("userInfo");
+      if (userInfo_str) {
+        this.userInfo = JSON.parse(userInfo_str);
+      }
+    },
+    changeComs() {
+      this.curCom = this.curCom === "reg" ? "login" : "reg";
     },
     submitInfo() {
       if (this.curCom === "reg") {
@@ -121,7 +151,41 @@ export default {
       });
       console.log(result, 92);
     },
-    _loginHandler() {},
+    async _loginHandler() {
+      let result = await request({
+        url: "/common/login",
+        method: "post",
+        data: {
+          userName: this.login.userName.value,
+          pwd: this.login.password.value,
+        },
+      });
+      if (result.status === 200 && result.data.status) {
+        // 登陆成功 存储信息
+        let userInfo = {
+          token: result.headers["authorization"],
+          userId: this.login.userName.value,
+          userName: this.login.userName.value,
+        };
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        this.userInfo = userInfo;
+        return;
+      }
+    },
+    async _getLoginInfo() {
+      let res = await request({
+        url: "/common/userTest",
+        method: "get",
+        headers: {
+          Authorization: this.userInfo.token,
+        },
+        data: {},
+      });
+      if (res.status === 200 && res.data.status === true) {
+        return true;
+      }
+      return false;
+    },
   },
 };
 </script>
@@ -164,12 +228,24 @@ input[type="password"] {
     color: #333;
   }
 }
-.submit-btn {
-  width: 55%;
-  margin-left: 110px;
-  line-height: 32px;
-  text-align: center;
-  background: rgba(55, 155, 195, 1);
-  color: #fff;
+.btns {
+  display: flex;
+  &-label {
+    flex-shrink: 1;
+    font-size: 12px;
+    line-height: 32px;
+    color: #999;
+    margin: 0 5px;
+  }
+  &-submit {
+    flex-shrink: 1;
+    flex: 1;
+    /* width: 55%; */
+    /* margin-left: 110px; */
+    line-height: 32px;
+    text-align: center;
+    background: rgba(55, 155, 195, 1);
+    color: #fff;
+  }
 }
 </style>
