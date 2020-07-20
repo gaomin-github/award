791d07c7e2bbd2dc19ea9d2c774e33540f09c313
gaomin-github <374204501@qq.com>
2020-07-20 13:12:24 +0800

    20-7-20 1312 增加权限页面

--- src/views/rbac-right/coms/right-index.vue
+++ src/views/rbac-right/coms/right-index.vue
@@ -1,16 +1,15 @@
 <template>
-<div class="list-wrapper">
-  list right
-</div>
+  <div class="list-wrapper">
+    <div class="bars"></div>
+    <div class="right"></div>
+  </div>
 </template>
 <script>
-export default{
-
-}
+export default {};
 </script>
 <style lang="scss" scoped>
-.list-wrapper{
-  width:100%;
-  height:100%;
+.list-wrapper {
+  width: 100%;
+  height: 100%;
 }
 </style>
