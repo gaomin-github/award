0dfafaa9ef763615139144ae52b80bd7ffe53d58
gaomin-github <374204501@qq.com>
2020-09-21 13:57:56 +0800

    20-9-21 增加柯里化处理

--- src/9_test/curry_func.js
+++ src/9_test/curry_func.js
@@ -1,5 +1,31 @@
+// 柯里化介绍：
+
+// 问题：
+// 1.并且返回接受余下的参数而且返回结果的新函数;如何在返回函数的同时也返回结果
+// 2.返回结果依赖调用函数本身有返回
 function curryFun(func) {
-  return function (params) {
-    console.log(params, 3)
-  }
+  let paramLen = func.length,
+    paramList = [];
+  curResult = null;
+  let cb = function() {
+    let args = Array.from(arguments);
+    console.log(args, 12);
+    paramList.push(...args);
+    if (paramList.length === paramLen) {
+      return func(...paramList);
+    } else {
+      return cb;
+    }
+  };
+  return cb;
 }
+
+function printName(a, b, c, d) {
+  let args = Array.from(arguments);
+  let result = args.join("_");
+  return result;
+}
+let newPrint = curryFun(printName);
+let newRes = newPrint("2", "5");
+
+console.log(newRes("3")("5"), 32);
