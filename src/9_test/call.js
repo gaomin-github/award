1191fee6b84bf7081d5ee0e77987f256943a280a
gaomin-github <374204501@qq.com>
2020-09-24 14:00:37 +0800

    20-9-24 1400 修改call方法实现

--- src/9_test/call.js
+++ src/9_test/call.js
@@ -7,13 +7,39 @@ function print() {
 }
 // print.call(foo);
 
-// 1.获取调用mycall的函数。利用原型/构造函数。this指向当前对象
-Function.prototype.myCall = function (obj, param) {
+// 1.获取调用mycall的函数。利用原型/构造函数。this指向当前对象(创建对象的研究？)
+function MyFunction(label) {
+  // this.label = label;
+  console.log(this.label, "label", 13);
+}
+// let MyFunction=new Function('label',console.log(this.lab))
+MyFunction.prototype.myCall = function() {
+  let params = Array.prototype.slice.call(arguments);
+  let curObj = params[0] || Window;
+  params.splice(0, 1);
+  curObj.fn = this;
+  // console.log(arguments.callee, 21);
+  // curObj.fn = arguments.callee;
 
-  obj.fn = this;
-  obj.fn();
-  // delete obj.fn;
+  curObj.fn(...params);
+  // delete curObj.fn;
 };
+// 原型实现对象
+function MyFunc1(label) {
+  console.log(this.label);
+}
+MyFunc1.prototype = new MyFunction();
+let funcObj1 = new MyFunc1("funcObj1");
+// myFunc1=new MyFunction("myFunc1");
+let dataObj = {
+  label: "dataObj",
+};
+funcObj1.myCall(dataObj, "age");
+function printCall() {
+  console.log(this.label, 31);
+}
+// printCall.myCall(dataObj, "name", "label");
+
 // 2.传参处理
 // call传参(obj, p1, p2, p3)
 // 可以用扩展运算法实现
@@ -36,5 +62,5 @@ function evalTest() {
 }
 // eval(a);
 // eval(`evalTest(${a})`);
-let b = { id: "123", name: "张三", age: 32 };
-eval(`evalTest(${b})`);
+// let b = { id: "123", name: "张三", age: 32 };
+// eval(`evalTest(${b})`);
