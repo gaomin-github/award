ccaec6440cec20fba638ea12bf7d06395907ca94
gaomin-github <374204501@qq.com>
2020-09-29 00:51:46 +0800

    20-9-29 0051 增加继承模拟说明

---
+++ src/9_test/extend_test.js
@@ -0,0 +1,25 @@
+function SuperObj(label) {
+  this.label = label;
+  console.log(this.label, "SuperObj");
+}
+
+function SubObj(label) {
+  console.log(label, 7);
+  this.subLabel = label;
+  console.log(this.subLabel, "SubObj");
+}
+console.log(SubObj === SubObj.prototype.constructor, 11);
+
+SubObj.prototype = new SuperObj("SubObj");
+let subInstance1 = new SubObj("instance1");
+console.log(subInstance1.constructor === SubObj, 15); //false
+console.log(subInstance1.constructor === SuperObj, 18); //true
+
+console.log(SubObj.constructor === SubObj, 20);
+console.log(SubObj.constructor === SuperObj, 21);
+// console.log(
+let fn = SubObj.constructor;
+fn("sub20");
+// console.log(fn, 20);
+// fn.call(this, "sub24");
+// );
