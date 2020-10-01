db72862f64154ba30d9b3cc3adbd30a676f72f3c
gaomin-github <374204501@qq.com>
2020-10-02 00:50:37 +0800

    20-10-2 0050 不可枚举属性遍历demo

---
+++ src/9_test/enumerable_test.js
@@ -0,0 +1,12 @@
+// 不可枚举属性遍历
+let enobj = {
+  id: "enobj",
+  label: "可枚举",
+};
+Object.defineProperty(enobj, "text", {
+  value: "不可枚举",
+});
+// 遍历不到不可枚举属性
+console.log(Object.keys(enobj), 9);
+// 可访问到
+console.log(Object.getOwnPropertyNames(enobj), 10);
