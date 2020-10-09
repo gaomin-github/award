dc37b8ea8b0ac3597829aa3c0f8af355a6bdc326
gaomin-github <374204501@qq.com>
2020-10-09 17:06:42 +0800

    20-10-9 1706 增加潜逃函数

--- src/9_test/nested_self.js
+++ src/9_test/nested_self.js
@@ -4,9 +4,31 @@
 //应该返回 3
 
 function myGet(obj, keyStr) {
-  let keyArr = keyStr.split(".");
+  let keyArr = keyStr.match(/\w+(?=(\.|\]|\[|\b))/g);
+  console.log(keyArr, 8);
   let curObj = obj;
-  for (let key in keyArr) {
-    curObj = curObj.key || {};
+
+  for (let i in keyArr) {
+    let key = keyArr[i];
+    curObj = curObj[key];
+    // console.log("key", key, "curObj", curObj);
+    if (!curObj) return null;
   }
+  return curObj;
 }
+let obj1 = {
+  info: {
+    items: [
+      {
+        id: 123,
+      },
+    ],
+    label: "myget test",
+  },
+  text: "myget test 1",
+};
+console.log(myGet(obj1, "info.items[0].id"), 28);
+// let getStr = "a[0].b.c";
+// console.log(getStr.split(/(\.|\[|\]/g));
+// js正则，匹配包围在【】或者.的数据
+// console.log(getStr.match(/\w+(?=(\.|\]|\[|\b))/g));
