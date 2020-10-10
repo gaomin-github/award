ff6e46310a42185c005da926c5dde5372fc56168
gaomin-github <374204501@qq.com>
2020-10-10 20:20:40 +0800

    20-10-10 2020 增加有序链表

--- src/9_test/linkedList_self.js
+++ src/9_test/linkedList_self.js
@@ -19,14 +19,14 @@ function singleLinkList() {
     val: null,
   };
 }
-singleLinkList.prototype.insert = function(val) {
+singleLinkList.prototype.insert = function (val) {
   let curNode = this.head;
   while (curNode.next) {
     curNode = curNode.next;
   }
   curNode.next = new singleNode(val);
 };
-singleLinkList.prototype.remove = function(val) {
+singleLinkList.prototype.remove = function (val) {
   let curNode = this.head;
   while (curNode.next) {
     let preNode = curNode;
@@ -38,7 +38,7 @@ singleLinkList.prototype.remove = function(val) {
     }
   }
 };
-singleLinkList.prototype.print = function() {
+singleLinkList.prototype.print = function () {
   let curNode = this.head;
   while (curNode.next) {
     curNode = curNode.next;
@@ -52,11 +52,10 @@ function addLink() {
   list1.insert(3);
   list1.insert(4);
 }
-addLink();
-// list1.print()
-
-list1.remove(4);
-console.log("remove-----------");
+// addLink();
+// // list1.print()
+// list1.remove(4);
+// console.log("remove-----------");
 // list1.print()
 
 function DoubleNode(val) {
@@ -74,7 +73,12 @@ function DoubleList() {
     next: null,
   };
 }
-DoubleList.prototype.insert = function(val) {
+DoubleList.prototype.init = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    this.insert(arr[i])
+  }
+}
+DoubleList.prototype.insert = function (val) {
   let curNode = this.head;
   while (curNode.next) {
     curNode = curNode.next;
@@ -84,15 +88,15 @@ DoubleList.prototype.insert = function(val) {
   curNode.next = newNode;
 };
 // 链表插入，指定插入位置
-DoubleList.prototype.insertIndex = function(val, index) {};
-DoubleList.prototype.print = function() {
+DoubleList.prototype.insertIndex = function (val, index) { };
+DoubleList.prototype.print = function () {
   let curNode = this.head;
   while (curNode.next) {
     curNode = curNode.next;
     console.log(curNode.val);
   }
 };
-DoubleList.prototype.remove = function(val) {
+DoubleList.prototype.remove = function (val) {
   let curNode = this.head;
   while (curNode.next) {
     let preNode = curNode;
@@ -107,12 +111,97 @@ DoubleList.prototype.remove = function(val) {
     }
   }
 };
-let list2 = new DoubleList();
-list2.insert(1);
-list2.insert(2);
-list2.insert(3);
-list2.insert(4);
-list2.print();
-console.log("double remove----");
-list2.remove(4);
-list2.print();
+// let list2 = new DoubleList();
+// list2.insert(1);
+// list2.insert(2);
+// list2.insert(3);
+// list2.insert(4);
+// // list2.print();
+// console.log("double remove----");
+// list2.remove(4);
+// // list2.print();
+
+function CycleDoubleList() {
+  // DoubleList.apply(this)
+  this.head = {
+    val: null,
+    pre: null,
+    next: null,
+  };
+  this.head.next = this.head
+}
+CycleDoubleList.prototype = DoubleList.prototype
+CycleDoubleList.prototype.constructor = CycleDoubleList;
+CycleDoubleList.prototype.insert = function (val) {
+  let curNode = this.head;
+  while (curNode.next && curNode.next.val !== null) {
+    curNode = curNode.next;
+  }
+  let newNode = new DoubleNode(val);
+  newNode.pre = curNode;
+  curNode.next = newNode;
+  newNode.next = this.head;
+  this.head.pre = newNode;
+}
+
+CycleDoubleList.prototype.print = function () {
+  let curNode = this.head;
+  // console.log(curNode, 139)
+  while (curNode.next && curNode.next.val !== null) {
+    curNode = curNode.next;
+    console.log(curNode.val);
+  }
+};
+
+CycleDoubleList.prototype.remove = function (val) {
+  let curNode = this.head;
+  while (curNode.next && curNode.next.val !== null) {
+    let preNode = curNode;
+    curNode = curNode.next;
+    // 引用类型比较，需要再改进
+    if (curNode.val === val) {
+      preNode.next = curNode.next;
+      if (curNode.next) {
+        curNode.next.pre = preNode;
+      }
+      return;
+    }
+  }
+};
+
+let list3 = new CycleDoubleList()
+list3.init([1, 2, 3, 4, 5])
+// console.log('CycleDoubleList remove---------')
+
+// list3.remove(4)
+// list3.print()
+
+function OrderLinkedList() {
+  CycleDoubleList.apply(this)
+}
+OrderLinkedList.prototype = CycleDoubleList.prototype;
+OrderLinkedList.prototype.constructor = OrderLinkedList;
+OrderLinkedList.prototype.insert = function (val) {
+  let curNode = this.head;
+  let newNode = new singleNode(val)
+  while (curNode.next && curNode.next.val !== null) {
+    let preNode = curNode;
+    curNode = curNode.next;
+    if (val < curNode.val) {
+      newNode.next = curNode;
+      curNode.pre = newNode;
+      newNode.pre = preNode
+      preNode.next = newNode;
+      return;
+    }
+  }
+
+  newNode.pre = curNode;
+  newNode.next = curNode.next;
+  newNode.next.pre = curNode;
+  curNode.next = newNode;
+}
+
+let orderList1 = new OrderLinkedList()
+orderList1.init([3, 5, 10, 9, 21, 14, 0])
+orderList1.print()
