56433788ed66d436eed4805d50302523873e749f
gaomin-github <374204501@qq.com>
2020-11-02 20:06:07 +0800

    20-11-2 2005 修改模板读取

--- build/cli-init-index.js
+++ build/cli-init-index.js
@@ -4,18 +4,23 @@ const mkdirp=require('mkdirp')
 module.exports=function(name){
     console.log(process.cwd(),'path')
     let newPjPath=path.join(process.cwd(),'../',name)
+    console.log(process.cwd(),7)
+    let curPjPath=process.cwd()
+    
     mkdirp(newPjPath,function(err){
-        templateList.forEach(template=>{
-            
-            if(template.type==='copy'){
-                if(fs.lstatSync(template.path)){
+        templateList.forEach(o=>{
+            if(o.type==='copy'){
+                if(fs.lstatSync(o.path)){
                     // 文件夹拷贝
                 }else{
                     // 文件拷贝
+                    // fs.writeFileSync(newPjPath)
+                    _handleConfigFile(fromPath,destPath)
                 }
-                fs.writeFileSync(newPjPath)
+            }else{
+                _handlePkgFile(path.join(process.cwd(),o.path),newPjPath)
+                    return;
             }
-            const {template,dir,name:filename}=require(template.path)
         })
         if(err){
             console.err('创建失败')
@@ -26,17 +31,57 @@ module.exports=function(name){
 }
 
 const templateList=[{
+
     label:'webpack',
     type:'copy',
-    path:'../config',
-
-
-},{
+    path:'./config/webpack.base.js',
+},
+// {
+//     label:'webpack',
+//     type:'copy',
+//     path:'./config',
+// },{
+//         label:'webpack',
+//         type:'copy',
+//         path:'./config',
+// },
+{
     label:'package',
     type:'compile',
-    path:'../package-template.json',
-},{
-    label:'dev template',
-    type:'copy',
-    path:'../template.dev.html',
-}]
+    path:'./package.json',
+}
+// ,{
+//     label:'dev template',
+//     type:'copy',
+//     path:'../template.dev.html',
+// }
+]
+
+// package.json相关
+function _handlePkgFile(fromPath,destPath){
+    console.log('copy file',56)
+    try{
+        const data=fs.readFileSync(fromPath,'utf-8')
+        const pkg=JSON.parse(data)
+        pkg.name='pj1'
+        pkg.version='1.0'
+        pkg.author='gaomin5'
+        delete pkg.scripts.myinit;
+        fs.writeFileSync(path.join(destPath,`./package_${Math.random()}.json`),JSON.stringify(pkg,null,2),'utf-8')
+    }catch(error){
+        console.log(red(`fail to generate:${error.message}`))
+        process.exit(1)
+    }  
+}
+
+function _handleConfigFile(fromPath,destPath){
+    console.log('copy file',56)
+
+    try{
+        const data=fs.readFileSync(fromPath,'utf-8')
+        fs.writeFileSync(path.join(destPath,'utf-8'),data)
+    }catch(error){
+        console.log(red(`fail to copy:${error.message}`))
+        process.exit(1)
+    }
+}
