dfb02f18547eebe0f53375ee2fdb2af46763d5c5
gaomin-github <374204501@qq.com>
2020-11-03 11:41:05 +0800

    20-11-3 1140 修改文件夹拷贝

--- build/cli-init-index.js
+++ build/cli-init-index.js
@@ -9,32 +9,37 @@ module.exports=function(name){
     
     mkdirp(newPjPath,function(err){
         templateList.forEach(o=>{
+            let fromPath=path.join(process.cwd(),o.path)
+            let destPath=path.join(newPjPath,o.path)
+            let stat=fs.lstatSync(fromPath)
             if(o.type==='copy'){
-                if(fs.lstatSync(o.path)){
+
+                if(stat.isDirectory()){
                     // 文件夹拷贝
+                    console.log('folder exist')
+                    _handleFolderTemplate(fromPath,destPath)
                 }else{
                     // 文件拷贝
                     // fs.writeFileSync(newPjPath)
                     _handleConfigFile(fromPath,destPath)
                 }
             }else{
-                _handlePkgFile(path.join(process.cwd(),o.path),newPjPath)
+                _handlePkgFile(fromPath,newPjPath)
                     return;
             }
         })
         if(err){
             console.err('创建失败')
         }else{
-            console.log('创建成功')
+            // console.log('创建成功')
         }
     })
 }
 
 const templateList=[{
-
     label:'webpack',
     type:'copy',
-    path:'./config/webpack.base.js',
+    path:'./config',
 },
 // {
 //     label:'webpack',
@@ -59,7 +64,7 @@ const templateList=[{
 
 // package.json相关
 function _handlePkgFile(fromPath,destPath){
-    console.log('copy file',56)
+    console.log('compile file',56)
     try{
         const data=fs.readFileSync(fromPath,'utf-8')
         const pkg=JSON.parse(data)
@@ -67,21 +72,49 @@ function _handlePkgFile(fromPath,destPath){
         pkg.version='1.0'
         pkg.author='gaomin5'
         delete pkg.scripts.myinit;
-        fs.writeFileSync(path.join(destPath,`./package_${Math.random()}.json`),JSON.stringify(pkg,null,2),'utf-8')
+        fs.writeFileSync(path.join(destPath,`./package.json`),JSON.stringify(pkg,null,2),'utf-8')
     }catch(error){
-        console.log(red(`fail to generate:${error.message}`))
+        console.log(`fail to generate:${error.message}`)
         process.exit(1)
     }  
 }
 
 function _handleConfigFile(fromPath,destPath){
     console.log('copy file',56)
+    console.log('fromPath',fromPath,'destPath',destPath,79)
 
     try{
         const data=fs.readFileSync(fromPath,'utf-8')
         fs.writeFileSync(path.join(destPath,'utf-8'),data)
     }catch(error){
-        console.log(red(`fail to copy:${error.message}`))
+        console.log(`fail to copy:${error.message}`)
         process.exit(1)
     }
+}
+/* 文件夾拷貝
+ */
+function _handleFolderTemplate(fromPath,destPath){
+    fs.access(destPath,function(err){
+        if(err){
+            // 创建文件夹
+            fs.mkdirSync(destPath);
+        }
+        fs.readdir(fromPath,(err,paths)=>{
+            if(err){
+                console.log('readdir error')
+            }else{
+                console.log(paths,106)
+                paths.forEach(o=>{
+                    
+                    let stat=fs.lstatSync(path.join(fromPath,o));
+                    if(stat.isDirectory()){
+                        _handleFolderTemplate(path.join(fromPath,o))
+                    }else{
+
+                        fs.writeFileSync(path.join(destPath,o),fs.readFileSync(path.join(fromPath,o)))
+                    }
+                })
+            }
+        })
+    })
 }
